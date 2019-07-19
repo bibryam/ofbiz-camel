9ef2ff3ed35225ff47df6acd90aeeb2cdff9a319
Girish Vasmatkar <girish.vasmatkar@hotwaxsystems.com>
2019-07-19 09:36:45 +0530

    Removed custome package scanner

--- src/main/java/org/apache/ofbiz/camel/loader/CamelContainer.java
+++ src/main/java/org/apache/ofbiz/camel/loader/CamelContainer.java
@@ -18,19 +18,16 @@
  *******************************************************************************/
 package org.apache.ofbiz.camel.loader;
 
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.DefaultPackageScanClassResolver;
 import org.apache.camel.impl.SimpleRegistry;
+import org.apache.camel.spi.PackageScanClassResolver;
 import org.apache.ofbiz.base.container.Container;
 import org.apache.ofbiz.base.container.ContainerConfig;
 import org.apache.ofbiz.base.container.ContainerException;
@@ -56,28 +53,19 @@ public class CamelContainer implements Container {
 		this.name = name;
 		context = createCamelContext();
 		ContainerConfig.Configuration cfg = ContainerConfig.getConfiguration(name, configFile);
-
 		String packageName = ContainerConfig.getPropertyValue(cfg, "package", "org.apache.ofbiz.camel.route");
-
-		try {
-			Class<?>[] classes = getClasses(packageName);
-			for (Class<?> classStr : classes) {
-				System.out.println("RouteBuilder found during scanning " + classStr.getName());
-
-				RouteBuilder routeBuilder = createRoutes(classStr.getName());
+		PackageScanClassResolver packageResolver = new DefaultPackageScanClassResolver();
+		Set<Class<?>> routesClassesSet = packageResolver.findImplementations(RouteBuilder.class, packageName);
+		routesClassesSet.forEach(key -> {
+			RouteBuilder routeBuilder;
+			try {
+				routeBuilder = createRoutes(key.getName());
 				addRoutesToContext(routeBuilder);
+			} catch (ContainerException e) {
 
 			}
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		});
 
-		// RouteBuilder routeBuilder = createRoutes();
-		// addRoutesToContext(routeBuilder);
 		producerTemplate = context.createProducerTemplate();
 
 	}
@@ -135,7 +123,6 @@ public class CamelContainer implements Container {
 
 	private RouteBuilder createRoutes(String routeBuilderClassName) throws ContainerException {
 		ClassLoader loader = Thread.currentThread().getContextClassLoader();
-
 		try {
 			Class<?> c = loader.loadClass(routeBuilderClassName);
 			return (RouteBuilder) c.newInstance();
@@ -150,69 +137,4 @@ public class CamelContainer implements Container {
 		return ServiceContainer.getLocalDispatcher("camel-dispatcher", delegator);
 //        return dispatcherFactory.createLocalDispatcher("camel-dispatcher", delegator);
 	}
-
-	/**
-	 * Scans all classes accessible from the context class loader which belong to
-	 * the given package and subpackages.
-	 *
-	 * @param packageName The base package
-	 * @return The classes
-	 * @throws ClassNotFoundException
-	 * @throws IOException
-	 */
-	@SuppressWarnings("unused")
-	private static Class<?>[] getClasses(String packageName) throws ClassNotFoundException, IOException {
-		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-		assert classLoader != null;
-		String path = packageName.replace('.', '/');
-		Enumeration<URL> resources = classLoader.getResources(path);
-		List<File> dirs = new ArrayList<File>();
-		while (resources.hasMoreElements()) {
-			URL resource = resources.nextElement();
-			dirs.add(new File(resource.getFile()));
-		}
-		ArrayList<Class> classes = new ArrayList<Class>();
-		for (File directory : dirs) {
-			classes.addAll(findClasses(directory, packageName));
-		}
-		return classes.toArray(new Class[classes.size()]);
-	}
-
-	/**
-	 * Recursive method used to find all classes in a given directory and subdirs.
-	 *
-	 * @param directory   The base directory
-	 * @param packageName The package name for classes found inside the base
-	 *                    directory
-	 * @return The classes
-	 * @throws ClassNotFoundException
-	 */
-	private static List<Class<? extends RouteBuilder>> findClasses(File directory, String packageName) throws ClassNotFoundException {
-		List<Class<? extends RouteBuilder>> classes = new ArrayList<Class<? extends RouteBuilder>>();
-		final String classExtension = ".class";
-		if (!directory.exists()) {
-			return classes;
-		}
-		File[] files = directory.listFiles(new FilenameFilter() {
-			public boolean accept(File dir, String name) {
-				return name.endsWith(".class") || dir.isDirectory();
-			}
-		});
-		for (File file : files) {
-			if (file.isDirectory()) {
-				assert !file.getName().contains(".");
-				classes.addAll(findClasses(file, packageName + "." + file.getName()));
-			} else if (file.getName().endsWith(".class")) {
-				String className = file.getName();
-				className = className.substring(0, className.length() - classExtension.length());
-				Class<?> cls = Class.forName(packageName + '.' + className);
-				if (RouteBuilder.class.isAssignableFrom(cls)) {
-					classes.add((Class<? extends RouteBuilder>) cls);
-				}
-
-			}
-		}
-		return classes;
-	}
-
 }
